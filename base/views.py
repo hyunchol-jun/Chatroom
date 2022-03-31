9b378a69b5f1ffb224495d4f8f97c61a8c8aabaa
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-03-31 12:24:08 -0400

    Add create room feature

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import RoomForm
 from .models import Room
 
 
@@ -14,3 +15,15 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
+
+
+def createRoom(request):
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    form = RoomForm()
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
