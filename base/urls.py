1972ff565e1052149167f78b3b0dd06d376bf37f
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-04-05 16:18:12 -0400

    Apply css styles

--- base/urls.py
+++ base/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
         "delete-message/<str:pk>/", views.deleteMessage, name="delete-message"
     ),
     path("profile/<str:pk>/", views.userProfile, name="user-profile"),
+    path("update-user/", views.updateUser, name="update-user"),
 ]
