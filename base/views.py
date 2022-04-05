357f733a20cfc42b1232fec7d0f7d16a1a119214
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-04-05 17:14:43 -0400

    Add mobile features

--- base/views.py
+++ base/views.py
@@ -71,7 +71,7 @@ def home(request):
         | Q(name__icontains=q)
         | Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {
@@ -195,3 +195,16 @@ def updateUser(request):
             form.save()
             return redirect("user-profile", pk=user.id)
     return render(request, "base/update-user.html", {"form": form})
+
+
+def topicsPage(request):
+    q = request.GET.get("q") if request.GET.get("q") is not None else ""
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, "base/topics.html", {"topics": topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(
+        request, "base/activity.html", {"room_messages": room_messages}
+    )
