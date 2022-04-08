2ce6f140d7cdae9d524222cecd78f0cc2d0eee5f
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-04-08 16:36:21 -0400

    Customize User model

--- base/admin.py
+++ base/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Message, Room, Topic
+from .models import Message, Room, Topic, User
 
 # Register your models here.
 
 
 admin.site.register(Room)
+admin.site.register(User)
 admin.site.register(Topic)
 admin.site.register(Message)
